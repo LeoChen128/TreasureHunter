c5ade911a350473c0fe95abd0fce68bb5657b5ba
BT_2S14_18 <leo1281515@gmai.com>
2025-01-06 11:01:41 -0500

    TH-2: Started working on the colors for the shop, (ex. modified the colors of the options to make it more appealing to the user). In addition, I modified the option to make it not case-sensitive.

--- src/Shop.java
+++ src/Shop.java
@@ -2,6 +2,7 @@
  * The Shop class controls the cost of the items in the Treasure Hunt game.<p>
  * The Shop class also acts as a go between for the Hunter's buyItem() method.<p>
  */
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Shop
@@ -83,13 +84,55 @@ public class Shop
      *
      * @return  the string representing the shop's items available for purchase and their prices
      */
+    public static final String reset = "\u001B[0m";
+    public static final String BLACK = "\033[0;30m";   // BLACK
+    public static final String RED = "\033[0;31m";     // RED
+    public static final String GREEN = "\033[0;32m";   // GREEN
+    public static final String YELLOW = "\033[0;33m";  // YELLOW
+    public static final String BLUE = "\033[0;34m";    // BLUE
+    public static final String PURPLE = "\033[0;35m";  // PURPLE
+    public static final String CYAN = "\033[0;36m";    // CYAN
+    public static final String WHITE = "\033[0;37m";   // WHITE
+    public static final String LIGHT_PURPLE = "\033[38;2;200;0;200m"; //LIGHT PURPLE
+    public static final String TEAL = "\033[38;2;0;225;221m"; //TEAL
+    public static final String ORANGE = "\033[38;2;225;153;0m"; //ORANGE
+    public static final String LIGHT_GREEN = "\033[38;2;136;255;0m"; //LIGHT GREEN
+    public static final String LIGHT_BLUE = "\033[38;2;120;172;255m"; //LIGHT BLUE
+    public static final String DARK_BLUE = "\033[38;2;72;0;255m"; //DARK BLUE
+    public static final String ROSY_PINK = "\033[38;2;255;0;162m"; //ROSY PINK
+    public static final String BROWN = "\033[38;2;135;82;62m"; //BROWN
+    public static final String FOREST_GREEN = "\033[38;2;62;135;81m"; //FOREST GREEN
+    public static final String BANANA_YELLOW = "\033[38;2;240;238;113m"; //BANANA YELLOW
+    public static final String DARK_RED = "\033[38;2;145;40;16m"; //DARK RED
+    public static final String LIGHT_PINK = "\033[38;2;255;153;240m"; //LIGHT PINK
+
+
+    public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
+    public static final String RED_BOLD = "\033[1;31m";    // RED
+    public static final String GREEN_BOLD = "\033[1;32m";  //  Darkish GREEN
+    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+    public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
+    public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
+    public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
+    public static final String GRAY_BOLD = "\033[1;37m";  // gray
+
+    public static final String BLACK_BRIGHT = "\033[0;90m";  // BLACK
+    public static final String RED_BRIGHT = "\033[0;91m";    // RED
+    public static final String GREEN_BRIGHT = "\033[0;92m";  // GREEN
+    public static final String YELLOW_BRIGHT = "\033[0;93m"; // YELLOW
+    public static final String BLUE_BRIGHT = "\033[0;94m";   // BLUE
+    public static final String PURPLE_BRIGHT = "\033[0;95m"; // PURPLE
+    public static final String CYAN_BRIGHT = "\033[0;96m";   // CYAN
+    public static final String WHITE_BRIGHT = "\033[0;97m";  // WHITE
+
+
     public String inventory()
     {
-        String str = "Water: " + WATER_COST + " gold\n";
-        str += "Rope: " + ROPE_COST + " gold\n";
-        str += "Machete: " + MACHETE_COST + " gold\n";
-        str += "Horse: " + HORSE_COST + " gold\n";
-        str += "Boat: " + BOAT_COST + " gold\n";
+        String str = CYAN_BOLD + "Water: " + reset + BLUE + WATER_COST + " gold" + reset +"\n";
+        str += BROWN + "Rope: " + reset + ROPE_COST + " gold" + reset + "\n";
+        str += YELLOW_BRIGHT + "Machete: " + reset + LIGHT_PINK + MACHETE_COST + " gold" + reset +"\n";
+        str += BROWN +  "Horse: " + reset + HORSE_COST + " gold" + reset +"\n";
+        str += ORANGE + "Boat: " + reset + BANANA_YELLOW + BOAT_COST + " gold" + reset +"\n";
 
         return str;
     }
@@ -154,23 +197,24 @@ public class Shop
      */
     public int getCostOfItem(String item)
     {
-        if (item.equals("Water"))
+        item = item.toLowerCase();
+        if (item.equals("water"))
         {
             return WATER_COST;
         }
-        else if (item.equals("Rope"))
+        else if (item.equals("rope"))
         {
             return ROPE_COST;
         }
-        else if (item.equals("Machete"))
+        else if (item.equals("machete"))
         {
             return MACHETE_COST;
         }
-        else if (item.equals("Horse"))
+        else if (item.equals("horse"))
         {
             return HORSE_COST;
         }
-        else if (item.equals("Boat"))
+        else if (item.equals("boat"))
         {
             return BOAT_COST;
         }
